f048db2c36da3b993e3ac788027f88ac52d986c1
nhontx <kevintran351996@gmail.com>
2023-12-25 20:52:03 +0700

    update

---
+++ server.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/users");
+const errorHandler = require("./middlewares/error");
+
+// Express App
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/users", userRoutes);
+
+app.use("/", (req, res) => {
+  return res.json({
+    message: "Welcome to the Node.js REST API using ExpressJS and MongoDB"
+  });
+});
+
+app.use(errorHandler);
+
+const startServer = () => {
+  try {
+    // Connect to DB
+    connectDB();
+
+    // Start & Listen to the requests
+    app.listen(port, () => console.log(`Server started listening on ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
